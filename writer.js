d6afcd3dcd9042330f3d28dccbcb45e04c229a2c
Artem Badrtdinov <bodrych@gmail.com>
2019-12-14 17:55:36 +0500

    write test

--- writer.js
+++ writer.js
@@ -2,17 +2,38 @@ const Influx = require('influx');
 const axios = require('axios');
 
 const influx = new Influx.InfluxDB({
-  host: 'localhost',
-  database: 'waves',
-  schema: [
-    {
-      measurement: 'pool',
-      fields: {
-        size: Influx.FieldType.INTEGER,
-      },
-      tags: [],
-    }
-  ]
+	host: 'localhost',
+	database: 'waves',
+	schema: [
+		{
+			measurement: 'pool',
+			fields: {
+				size: Influx.FieldType.INTEGER,
+			},
+			tags: [],
+		}
+	]
 });
 
-influx.getDatabaseNames().then(console.log)
+const checkPool = async () => {
+	try {
+		const size = (await axios.get('https://nodes.wavesnodes.com/transactions/unconfirmed/size')).size;
+		await influx.writePoints(
+			[
+				{
+					measurement: 'pool',
+					fields: {
+						size,
+					},
+				}
+			],
+			{
+				retentionPolicy: '1h',
+			}
+		);
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+setInterval(checkPool, 5000);
